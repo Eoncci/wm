0582c7de8f578ee08737c74aa1ca6dfab02f803d
xmoyking <xmoyking@gmail.com>
2017-07-17 09:21:48 +0800

    添加表单验证

--- static/assets/js/application.js
+++ static/assets/js/application.js
@@ -1,5 +1,13 @@
 $(function() {
 
+  // 验证手机号
+  function checkPhone(phone){
+      if(!(/^1[34578]\d{9}$/.test(phone))){
+          return false;
+      }
+      return true;
+  }
+
   /*-----------------------------------------------------------------------------------*/
   /*  Anchor Link
   /*-----------------------------------------------------------------------------------*/
@@ -26,17 +34,59 @@ $(function() {
   // 预约申请
   $('#signform').submit(function (e) {
       e.preventDefault();
+      if( !checkPhone($('#tel').value()) ){
+          swal({
+            html: true ,
+            title: '提示',
+            text: '电话号码格式不正确，请填写手机号码', 
+            type: 'warning',
+            confirmButtonText: '确定', 
+          });
+          $('#tel').focus();
+          return false;
+      }
+      if( !$.trim($('#name').value()) ){
+          swal({
+            html: true ,
+            title: '提示',
+            text: '请填写姓名', 
+            type: 'warning',
+            confirmButtonText: '确定', 
+          });
+          $('#name').focus();
+          return false;
+      }
+      if( !$.trim($('#address').value()) ){
+          swal({
+            html: true ,
+            title: '提示',
+            text: '请填写姓名', 
+            type: 'warning',
+            confirmButtonText: '确定', 
+          });
+          $('#address').focus();
+          return false;
+      }
+
       $.post('/washingmachine/wm_order', $('#signform').serialize() ,function (res) {
         console.log(res);
-        alert('预约成功', res);
-        // var temp = $(
-        //      '<div class="dn"><p>正在派送中，</p>\
-        //       <p>联系人姓名：'+ res.tel +'</p>\
-        //       <p>房间地址为：</p>\
-        //       <p>租赁时间为：'+ res.star + ' - ' + res.end +'</p></div>'
-        //     );
-        // $('#statusinfo').append(temp);
-        // temp.fadeIn();
+        if(res.match('success')){
+          swal({ 
+            html: true ,
+            title: '预约成功',
+            text: '可通过电话号码<a href="/washingmachine/status">查看申请状态</a>', 
+            type: 'success',
+            confirmButtonText: '确定', 
+          });
+        }else{
+          swal({
+            html: true ,
+            title: '提示',
+            text: JSON.parse(res).error, 
+            type: 'warning',
+            confirmButtonText: '确定', 
+          });
+        }
       });
   });
 
@@ -44,13 +94,25 @@ $(function() {
   // 获取预约状态
   $('#statusform').submit(function (e) {
       e.preventDefault();
+      if( !checkPhone($('#tel').value()) ){
+          swal({
+            html: true ,
+            title: '提示',
+            text: '电话号码格式不正确，请填写手机号码', 
+            type: 'warning',
+            confirmButtonText: '确定', 
+          });
+          $('#tel').focus();
+          return false;
+      }
       $.post('/washingmachine/query', $('#statusform').serialize() ,function (res) {
+        res = JSON.parse(res);
         console.log(res);
         var temp = $(
              '<div class="dn"><p>正在派送中，</p>\
-              <p>联系人姓名：'+ res.tel +'</p>\
+              <p>联系人电话：'+ res.tel +'</p>\
               <p>房间地址为：</p>\
-              <p>租赁时间为：'+ res.star + ' - ' + res.end +'</p></div>'
+              <p>租赁时间为：'+ res.start + ' - ' + res.end +'</p></div>'
             );
         $('#statusinfo').append(temp);
         temp.fadeIn();
