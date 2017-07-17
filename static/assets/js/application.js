d392b3e3ec077bf84aec755aff27024a44b576a9
xmoyking <xmoyking@gmail.com>
2017-07-17 08:36:40 +0800

    ajax获取预约状态

--- static/assets/js/application.js
+++ static/assets/js/application.js
@@ -23,4 +23,20 @@ $(function() {
   /*-----------------------------------------------------------------------------------*/
   $('.tooltip-side-nav a').tooltip();
   
+  // 获取预约状态
+  $('#statusform').submit(function (e) {
+      e.preventDefault();
+      $.post('/washingmachine/query',function (res) {
+        console.log(res);
+        var temp = $(
+             '<div class="dn"><p>正在派送中，</p>\
+              <p>联系人姓名：'+ res.tel +'</p>\
+              <p>房间地址为：</p>\
+              <p>租赁时间为：'+ res.star + ' - ' + res.end +'</p></div>'
+            );
+        $('#statusinfo').append(temp);
+        temp.fadeIn();
+      });
+  });
+
 });
