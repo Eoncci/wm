5ca52e01e43636b5b89b6ae0ad5777b56eee26fc
xmoyking <xmoyking@gmail.com>
2017-07-17 08:49:30 +0800

    测试ajax

--- static/assets/js/application.js
+++ static/assets/js/application.js
@@ -23,10 +23,28 @@ $(function() {
   /*-----------------------------------------------------------------------------------*/
   $('.tooltip-side-nav a').tooltip();
   
+  // 预约申请
+  $('#signform').submit(function (e) {
+      e.preventDefault();
+      $.post('/washingmachine/wm_order', $('#signform').serialize() ,function (res) {
+        console.log(res);
+        alert('预约成功', res);
+        // var temp = $(
+        //      '<div class="dn"><p>正在派送中，</p>\
+        //       <p>联系人姓名：'+ res.tel +'</p>\
+        //       <p>房间地址为：</p>\
+        //       <p>租赁时间为：'+ res.star + ' - ' + res.end +'</p></div>'
+        //     );
+        // $('#statusinfo').append(temp);
+        // temp.fadeIn();
+      });
+  });
+
+
   // 获取预约状态
   $('#statusform').submit(function (e) {
       e.preventDefault();
-      $.post('/washingmachine/query',function (res) {
+      $.post('/washingmachine/query', $('#statusform').serialize() ,function (res) {
         console.log(res);
         var temp = $(
              '<div class="dn"><p>正在派送中，</p>\
@@ -38,5 +56,7 @@ $(function() {
         temp.fadeIn();
       });
   });
+
+
 
 });
