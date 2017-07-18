4ecf26d6b958d60a9081b037fa943c9195969ea9
mengfanchen <1034406291@qq.com>
2017-07-18 12:59:15 +0800

    fix some '/' bug and debug=false

--- washingmachine/admin.py
+++ washingmachine/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Order)
+admin.site.register(Userinfo)
+admin.site.register(Wm)
 # Register your models here.
--- washingmachine/urls.py
+++ washingmachine/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     url(r'^order$', views.order, name='order'),
     url(r'^status$', views.status, name='status'),
     url(r'^wm_order$', views.wm_order, name='wm_order'),
